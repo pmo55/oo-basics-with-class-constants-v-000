2e2e9144bcbc614cead9f751af9d36a97e6c8a62
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-10 23:59:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  
 GENRES = []
   def initialize(title)
     @title = title
@@ -9,7 +10,10 @@ GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+def genre=(genre)
+  @genre=genre
+  GENRE << genre
+end
 
 
 end
